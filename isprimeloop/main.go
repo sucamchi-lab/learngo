ab20424f5f321147960829d7a32454edcd8b14e9
sucamchi-lab <sucamchi@gmail.com>
2026-04-04 12:17:52 +0200

    is_prime

---
+++ isprimeloop/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printPrimes(max int) {
+	for n := 2; n <= max; n++ {
+		if n == 2 {
+			fmt.Printf("%v ", n)
+			continue
+		}
+		if n%2 == 0 {
+			continue
+		}
+		isPrime := true
+		for i := 3; i*i <= n; i += 2 {
+			if n%i == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			fmt.Printf("%v ", n)
+		}
+	}
+}
+
+// don't edit below this line
+
+func test(max int) {
+	fmt.Printf("Primes up to %v:\n", max)
+	printPrimes(max)
+	fmt.Println("===============================================================")
+}
+
+func main() {
+	test(10)
+	test(20)
+	test(30)
+}
